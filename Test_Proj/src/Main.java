00f2fffc71b87f52ffc2cdd602674f51ea55e494
garg <dipti.garg@siemens.com>
2014-09-18 11:24:16 +0530

    Initial commit

---
+++ Test_Proj/src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        DemoData dd = new DemoData();
+        
+        displayGreeting();
+        dd.display();
+    }
+    
+    private static void displayGreeting()
+    {
+        System.out.println("HI...");
+        System.out.println("Welcome to GIT"); 
+      
+    }
+}
